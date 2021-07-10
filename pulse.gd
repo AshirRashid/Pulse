72629a7aaad6d5bd30b19154aca4dc0c4f36967c
AshirRashid <64663586+AshirRashid@users.noreply.github.com>
2021-07-10 15:45:22 +0500

    Update pulse.gd

--- pulse.gd
+++ pulse.gd
@@ -1,10 +1,12 @@
-func pulse_between(start, target, line_num=2, points_num=15):
-	var relative_target = self.to_local(target)
+func pulse_between(start, end, line_num=2, points_num=15):
+	# lin_num: number of polylines to be drawn when this function is called
+	# points_num: number of points in each line
+	var relative_end = self.to_local(end)# position of the end point relative to the canvas_item whose draw function is used to call this function
 	var relative_start = self.to_local(start)
 	var points = [relative_start]
-	var relative_target2start = relative_target-relative_start
-	var dir = relative_target2start.normalized()
-	var segment_length = relative_target2start.length()/points_num
+	var relative_end2start = relative_end-relative_start
+	var dir = relative_end2start.normalized()
+	var segment_length = relative_end2start.length()/points_num
 	
 	for j in range(line_num):
 		for i in range(1, points_num-1):
@@ -12,6 +14,6 @@ func pulse_between(start, target, line_num=2, points_num=15):
 			var current_point = relative_start + i*dir*segment_length + point_offset*Vector2(-dir.y, dir.x)
 			points.append(current_point)
 			off += 100
-		points.append(relative_target)
+		points.append(relative_end)
 		draw_polyline(points, Color('#DDDDDD'), randi()%5 +2
 		)
