ab5e86fed5d8fb493e98bf944734003a427fd35a
AshirRashid <64663586+AshirRashid@users.noreply.github.com>
2021-11-14 14:43:40 +0500

    Update pulse.gd

--- pulse.gd
+++ pulse.gd
@@ -1,3 +1,35 @@
+# IMPLEMENTATION WITH Line2D
+
+func pulse_between(line2d: Line2D, start:Vector2, end:Vector2, line_num:=3, points_num:=20, max_amp:=20):
+	# line2d: the Line2D node to be used to display the lines
+	# start: the originating point of all lines
+	# end: the ending point of all lines
+	# line_num: number of polylines to be drawn when this function is called
+	# points_num: number of points in each line
+	# max_amp: every point's maximum perpendicular distance from the line
+	var end2start = end-start
+	var dir = end2start.normalized()
+	var segment_length = end2start.length()/points_num
+	line2d.points = [start]
+	
+	for j in range(line_num):
+		var pulse_points = PoolVector2Array()
+		for i in range(1, points_num):
+			var point_offset = (randi()%max_amp*2)-max_amp
+			var current_point = start + i*dir*segment_length + point_offset*Vector2(-dir.y, dir.x)
+			# Notice that Vector2(-dir.y, dir.x) is perpendicular to dir
+			pulse_points.append(current_point)
+			
+		if j%2 == 0:
+			pulse_points.append(end)
+		else:
+			pulse_points.invert()
+			pulse_points.append(start)
+		line2d.points += pulse_points
+
+
+# IMPLEMENTATION WITH draw_polyline
+
 func pulse_between(start, end, line_num=2, points_num=15):
 	# line_num: number of polylines to be drawn when this function is called
 	# points_num: number of points in each line
